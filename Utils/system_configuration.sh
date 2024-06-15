10ac2f9ca4ded808ca86b2219668b3a47f1303be
Loren Stone <97418895+AncientLor@users.noreply.github.com>
2024-06-14 22:21:52 -0700

    Update system_configuration.sh

--- Utils/system_configuration.sh
+++ Utils/system_configuration.sh
@@ -1,2 +1,6 @@
 # Prevent Sleep/Suspend/Hibernate (Clamshell) 
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# List/Modify Bootorder
+sudo efibootmgr
+sudo efibootmgr -o <first>,<second>
